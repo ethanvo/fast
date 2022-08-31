a56d3a24a3c947d77613f4b4374064125992506d
Ethan Vo <ethan.vo@hotmail.com>
2022-08-30 21:09:08 -0400

    black

--- hartree_fock.py
+++ hartree_fock.py
@@ -11,15 +11,17 @@ from math_tools import get_Fock_G, get_density_matrix, get_electronic_energy
 mol = gto.Mole()
 mol.verbose = 7
 mol.atom = [
-        ['O', (  0.0000,  0.0000,  0.116)],
-        ['H', (  0.0000,  0.749, -0.463)],
-        ['H', (  0.0000, -0.749, -0.463)]]
-mol.basis = 'ccpvdz'
+    ["O", (0.0000, 0.0000, 0.116)],
+    ["H", (0.0000, 0.749, -0.463)],
+    ["H", (0.0000, -0.749, -0.463)],
+]
+mol.basis = "ccpvdz"
 mol.build()
 
 E_nuc = mol.energy_nuc()
 
 from pyscf import scf
+
 mymf = scf.RHF(mol)
 mymf.kernel()
 
@@ -29,10 +31,10 @@ mymf.kernel()
 nbas = mol.nao_nr()
 nelectron = mol.nelectron
 
-S = gto.getints('cint1e_ovlp_sph', mol._atm, mol._bas, mol._env)
-V_nucl = gto.getints('cint1e_nuc_sph', mol._atm, mol._bas, mol._env)
-T = gto.getints('cint1e_kin_sph', mol._atm, mol._bas, mol._env)
-eris = gto.getints('cint2e_sph', mol._atm, mol._bas, mol._env)
+S = gto.getints("cint1e_ovlp_sph", mol._atm, mol._bas, mol._env)
+V_nucl = gto.getints("cint1e_nuc_sph", mol._atm, mol._bas, mol._env)
+T = gto.getints("cint1e_kin_sph", mol._atm, mol._bas, mol._env)
+eris = gto.getints("cint2e_sph", mol._atm, mol._bas, mol._env)
 
 H_core = V_nucl + T
 
@@ -41,8 +43,9 @@ H_core = V_nucl + T
 # Canonical orthogonalization
 # S is Hermitian
 ###############################################################################
-from math_tools import symmetric_orthogonalization
-X = symmetric_orthogonalization(S)
+from math_tools import canonical_orthogonalization
+
+X = canonical_orthogonalization(S)
 
 ###############################################################################
 # Obtain a guess density matrix P
@@ -53,49 +56,49 @@ P = np.zeros((nbas, nbas), dtype=dtype)
 conv = False
 
 while not conv:
-###############################################################################
-# Calculate matrix G from P and eris
-###############################################################################
+    ###############################################################################
+    # Calculate matrix G from P and eris
+    ###############################################################################
     G = get_Fock_G(P, eris)
 
-###############################################################################
-# Add G to core Hamiltonian to obtain the Fock matrix F = H_core + G
-###############################################################################
+    ###############################################################################
+    # Add G to core Hamiltonian to obtain the Fock matrix F = H_core + G
+    ###############################################################################
     F = H_core + G
 
-###############################################################################
-# Calculate the transformed Fock matrix F' = X^\dagFX
-###############################################################################
+    ###############################################################################
+    # Calculate the transformed Fock matrix F' = X^\dagFX
+    ###############################################################################
     F_prime = multi_dot([X.conj().T, F, X])
 
-###############################################################################
-# Diagonalize F' to obtain C' and epsilon
-###############################################################################
+    ###############################################################################
+    # Diagonalize F' to obtain C' and epsilon
+    ###############################################################################
     epsilon, C_prime = eigh(F_prime)
     index_array = np.argsort(epsilon)
     epsilon = epsilon[index_array]
     C_prime = C_prime[:, index_array]
     print(epsilon)
-###############################################################################
-# Calculate C = XC'
-###############################################################################
+    ###############################################################################
+    # Calculate C = XC'
+    ###############################################################################
     C = np.dot(X, C_prime)
 
-###############################################################################
-# P_{mu, nu} = 2\sum^{\frac{N}{2}}_aC_{mu, a}C^*{nu, a}
-###############################################################################
+    ###############################################################################
+    # P_{mu, nu} = 2\sum^{\frac{N}{2}}_aC_{mu, a}C^*{nu, a}
+    ###############################################################################
     P_prime = get_density_matrix(C, nelectron)
     E_0 = get_electronic_energy(P_prime, H_core, F)
     E_tot = E_0 + E_nuc
     print(E_tot)
 
-###############################################################################
-# Check Convergence delta less than 1.0E-4
-###############################################################################
+    ###############################################################################
+    # Check Convergence delta less than 1.0E-4
+    ###############################################################################
     if (np.sum(np.abs(P_prime - P)) / nbas) < 1e-8:
         conv = True
     else:
         P = P_prime
 
-S = np.array([[1.0, 0.4508],[0.4508, 1.0]])
-print(symmetric_orthogonalization(S))
+S = np.array([[1.0, 0.4508], [0.4508, 1.0]])
+print(canonical_orthogonalization(S))
--- math_tools.py
+++ math_tools.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python
-###############################################################################
-# Canonical Orthogonalization
-# X = Us^{-\frac{1}{2}}
-# S is Hermitian
-###############################################################################
 from scipy.linalg import eigh
 from numpy.linalg import multi_dot
 from numpy import dot
+import numpy as np
+
+
 def symmetric_orthogonalization(S):
     s, U = eigh(S)
-    X = multi_dot([U, np.diag(s ** -0.5), U.conj().T])
-    print(X)
+    X = multi_dot([U, np.diag(s**-0.5), U.conj().T])
     return X
 
-###############################################################################
-# Symmetric Orthogonalization
-# 
-###############################################################################
+
 def canonical_orthogonalization(S):
     s, U = eigh(S)
-    X = dot(U, np.diag(s ** -0.5))
-    print(X)
+    X = dot(U, np.diag(s**-0.5))
     return X
 
-###############################################################################
-# Get two electron part of the Fock matrix G
-# G_{mu,nu} = \sum_{lam,sig}[(mu, nu|sig, lam) - \frac{1}{2}(mu, lam|sig, nu)]
-###############################################################################
-import numpy as np
+
+
+
 def get_Fock_G(P, eris):
     dtype = eris.dtype
     nbas = P.shape[0]
@@ -36,13 +27,14 @@ def get_Fock_G(P, eris):
         for nu in range(nbas):
             for lam in range(nbas):
                 for sig in range(nbas):
-                    G[mu, nu] = G[mu, nu] + P[lam, sig] * (eris[mu, nu, sig, lam] - 0.5 * eris[mu, lam, sig, nu])
+                    G[mu, nu] = G[mu, nu] + P[lam, sig] * (
+                        eris[mu, nu, sig, lam] - 0.5 * eris[mu, lam, sig, nu]
+                    )
     return G
 
-###############################################################################
-# Get density matrix, P P_{mu, nu} = 2\sum^{\frac{N}{2}}_aC_{mu, a}C^*{nu, a}
-###############################################################################
-import numpy as np
+
+
+
 def get_density_matrix(C, nelectron):
     nbas = C.shape[0]
     dtype = C.dtype
@@ -53,11 +45,9 @@ def get_density_matrix(C, nelectron):
                 P[mu, nu] = P[mu, nu] + 2 * C[mu, a] * C.conj()[nu, a]
     return P
 
-###############################################################################
-# Get electronic energy
-# E_0 = \frac{1}{2}\sum_{mu}\sum_{nu}P_{nu mu}(H_core_{mu nu} + F_{mu nu})
-###############################################################################
-import numpy as np
+
+
+
 def get_electronic_energy(P, H_core, F):
     nbas = P.shape[0]
     E_0 = 0
